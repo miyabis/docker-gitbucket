e9ae04ecf97d8cd23329692c245342a4baca4082
MiYABiS <miyabis@live.jp>
2018-06-11 22:42:53 +0900

    Mod Env

--- 4/entrypoint.sh
+++ 4/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 if [ "$DB_URL" -a "$DB_USER" -a "$DB_PASSWORD" ]; then
-  cat > /gitbucket/database.conf <<-EOCONF
+cat > /gitbucket/database.conf <<-EOCONF
 db {
   url = "$DB_URL"
   user = "$DB_USER"
@@ -10,4 +10,4 @@ db {
 EOCONF
 fi
 
-exec java -jar /opt/gitbucket.war --gitbucket.home=/gitbucket $OPTS
+exec java -jar /opt/gitbucket.war --gitbucket.home=/gitbucket
